aa4496e04603e82afabe65e3491b134f2a0c8b9a
arjun <sarodearjun57@gmail.com>
2015-10-16 14:45:26 -0500

    initial prototype

--- sketch_sep14a.c/sketch_sep14a/sketch_sep14a.ino
+++ sketch_sep14a.c/sketch_sep14a/sketch_sep14a.ino
@@ -1,14 +1,43 @@
 const int analogInPin = A0;
+const int vibePin1 = 8;
 int sensorValue = 0;
+int flexInitial;
+int flexChange;
+int purrCount = 0;
+int purrThresh = 100;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
   pinMode(0, INPUT_PULLUP);
+  flexInitial = analogRead(analogInPin);
+  Serial.print("INITIAL FLEX VALUE: ");
+  Serial.println(flexInitial);
 }
 
 void loop() {
   sensorValue = analogRead(analogInPin);
+  flexChange = abs(flexInitial - sensorValue);
+  vibrate(flexChange);
+  analogWrite(9, flexChange);
   // print the results to the serial monitor:
   Serial.print("sensor = " );                       
-  Serial.println(sensorValue/10);      
+  Serial.println(sensorValue);
+  delay(100);
+}
+
+void vibrate(int change) {
+  if (change > purrThresh) {
+    purrCount++;
+    Serial.print("purr count = ");
+    Serial.println(purrCount);
+  }
+  
+  if (purrCount > 100) {
+    purrCount = 0;
+    digitalWrite(vibePin1, HIGH);
+    delay(2000);
+    digitalWrite(vibePin1, LOW);
+  }
 }
