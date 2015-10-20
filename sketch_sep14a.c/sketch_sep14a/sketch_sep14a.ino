df25999203f8d8ad51aa8b74b2207d7ccd06a581
arjun <sarodearjun57@gmail.com>
2015-10-20 18:51:00 -0500

    waits for sensor to steady

--- sketch_sep14a.c/sketch_sep14a/sketch_sep14a.ino
+++ sketch_sep14a.c/sketch_sep14a/sketch_sep14a.ino
@@ -4,7 +4,14 @@ int sensorValue = 0;
 int flexInitial;
 int flexChange;
 int purrCount = 0;
-int purrThresh = 100;
+int purrCountGoal = 35;
+int purrThresh = 10;
+
+int steadyReading;
+int steadyCount = 0;
+int steadyThresh = 3;
+int steadyCountGoal = 100;
+boolean isSteady = false;
 
 void setup() {
   Serial.begin(9600);
@@ -18,12 +25,16 @@ void setup() {
 
 void loop() {
   sensorValue = analogRead(analogInPin);
-  flexChange = abs(flexInitial - sensorValue);
-  vibrate(flexChange);
-  analogWrite(9, flexChange);
-  // print the results to the serial monitor:
   Serial.print("sensor = " );                       
   Serial.println(sensorValue);
+  if (isSteady == false) {
+    isSteady = sensorIsSteady(sensorValue);
+    return;
+  }
+  flexChange = abs(flexInitial - sensorValue);
+  
+  vibrate(flexChange);
+  analogWrite(9, flexChange);
   delay(100);
 }
 
@@ -34,10 +45,41 @@ void vibrate(int change) {
     Serial.println(purrCount);
   }
   
-  if (purrCount > 100) {
+  if (purrCount > purrCountGoal) {
     purrCount = 0;
-    digitalWrite(vibePin1, HIGH);
-    delay(2000);
-    digitalWrite(vibePin1, LOW);
+    resetSteady();
+    for (int i = 0; i < random(3, 6); i++) {
+      pulse();
+    }
+  }
+}
+
+void pulse() {
+  delay(1000);
+  digitalWrite(vibePin1, HIGH);
+  delay(3000);
+  digitalWrite(vibePin1, LOW);
+}
+
+void resetSteady() {
+  steadyCount = 0;
+  isSteady = false;
+}
+
+boolean sensorIsSteady(int sensor) {
+  int diff = abs(sensor - flexInitial);
+  if (diff > 10) {
+    Serial.print("UPDATED FLEX INITIAL: ");
+    Serial.println(flexInitial);
+    flexInitial = sensor;
+  }
+  else if (diff < steadyThresh) {
+    steadyCount++;
+  }
+  if (steadyCount >= steadyCountGoal) {
+    Serial.println("SENSOR IS STEADY");
+    return true;
+  } else {
+    return false;
   }
 }
